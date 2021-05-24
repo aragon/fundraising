f61db01950418a3528161fdcd2dbe3afd5e1c05c
GurakG <48178500+GurakG@users.noreply.github.com>
2021-05-24 11:59:28 +0300

    DAI and ANT upgrade
    Upgraded ANT contract address on Main Network.
    Upgraded DAI contract address on Rinkeby to be the same across all Aragon products.

--- apps/aragon-fundraising/app/src/constants/tokens.js
+++ apps/aragon-fundraising/app/src/constants/tokens.js
@@ -4,10 +4,10 @@
 export const Tokens = {
   main: {
     DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
-    ANT: '0x960b236a07cf122663c4303350609a66a7b288c0',
+    ANT: '0xa117000000f279d81a1d3cc75430faa017fa5a2e',
   },
   rinkeby: {
-    DAI: '0x0527e400502d0cb4f214dd0d2f2a323fc88ff924',
+    DAI: '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa',
     ANT: '0x0d5263b7969144a852d58505602f630f9b20239d',
   },
 }
